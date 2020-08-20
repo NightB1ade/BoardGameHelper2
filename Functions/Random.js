df855dc38acf4984f6c6b108aa4e3fece3c47d8c
NightB1ade <NightB1ade@users.noreply.github.com>
2020-08-20 22:50:40 +0100

    Initial Load

---
+++ Functions/Random.js
@@ -0,0 +1,56 @@
+function getRandomInteger(min,max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+
+
+
+function getRandomIntegerExcludingOne(min,max,excluding) {
+	var output = getRandomInteger(min,max - 1);
+	if (output == excluding) {
+		return max
+	} else {
+		return output
+	}
+}
+
+
+
+
+function ShuffleArray(array) {
+	var i = 0;
+	var j = 0;
+	var temp = null;
+
+	for (
+		i = array.length - 1
+		; i > 0
+		; i -= 1
+	) {
+		j = Math.floor(Math.random() * (i + 1));
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	return array
+}
+
+
+
+
+function getRandomIntArray(min,max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	var array = [];
+
+	for (i = min ; i <= max ; i += 1) {
+		array.push(i);
+	}
+
+	array = ShuffleArray(array);
+
+	return array
+}
